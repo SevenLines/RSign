b205826fe2dfb57a557f46b724d9d117eb4a22cf
SevenLines <mmailm@mail.ru>
2013-11-20 01:19:00 +0900

    добавил фильтр DAV при выборе видео

--- RSign_TLB.h
+++ RSign_TLB.h
@@ -10,7 +10,7 @@
 // ************************************************************************ //
 
 // C++ TLBWRTR : $Revision:   1.151.1.0.1.27  $
-// File generated on 14.11.2013 2:03:36 from Type Library described below.
+// File generated on 20.11.2013 1:11:53 from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\_DISTR\_ASUDOR\RSign\RSign.tlb (1)
