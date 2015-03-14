0c004145720cedefe940d870e63be4fdeda26c51
ISTU <mmailm@mail.ru>
2015-03-14 17:27:56 +0800

    remove topmost

--- RSign_TLB.cpp
+++ RSign_TLB.cpp
@@ -10,7 +10,7 @@
 // ************************************************************************ //
 
 // C++ TLBWRTR : $Revision:   1.151.1.0.1.27  $
-// File generated on 10.03.2015 20:17:55 from Type Library described below.
+// File generated on 14.03.2015 17:26:24 from Type Library described below.
 
 // ************************************************************************  //
 // Type Lib: D:\Projects\RSign\RSign.tlb (1)
