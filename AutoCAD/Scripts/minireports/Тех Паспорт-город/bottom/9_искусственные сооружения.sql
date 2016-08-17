cebd494cff6611b53521a5a1399fd2cefa471ac2
mick <mmailm@mail.ru>
2016-08-18 03:54:54 +0800

    add sqltotxt sources

---
+++ AutoCAD/Scripts/minireports/Тех Паспорт-город/bottom/9_искусственные сооружения.sql
@@ -0,0 +1,12 @@
+DECLARE @NumDataSource int
+SELECT @NumDataSource = /*$NumDataSource*/3742/*$*/
+
+SET NOCOUNT ON 
+SELECT cast(Displacement*100 as int), 
+100, 
+'$b tubeEx  style=vertical iLength:L='+cast(extent as varchar(10))
++ ' Position:' + dbo.pp_Km_mFormat(Displacement) 
++ ' DOut:' + TubeScheme
+FROM ListTubes
+WHERE NumDataSource = @NumDataSource
+ORDER BY 1
